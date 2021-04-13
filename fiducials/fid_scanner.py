792df7c2f0528fb7a3f3d14e8f476973a8e15720
Dan <dannyhudetz@gmail.com>
2021-04-13 18:46:06 -0500

    cleaned up fiducials

--- fiducials/fid_scanner.py
+++ fiducials/fid_scanner.py
@@ -45,16 +45,25 @@ while True:
 
 			# draw the bounding box of the ArUCo detection
 			cv2.line(frame, topLeft, topRight, (0, 255, 0), 2)
-			cv2.line(frame, topRight, bottomRight, (0, 255, 0), 2)
-			cv2.line(frame, bottomRight, bottomLeft, (0, 255, 0), 2)
-			cv2.line(frame, bottomLeft, topLeft, (0, 255, 0), 2)
+			cv2.line(frame, topRight, bottomRight, (0, 0, 255), 2)
+			cv2.line(frame, bottomRight, bottomLeft, (255, 0, 0), 2)
+			cv2.line(frame, bottomLeft, topLeft, (200, 200, 0), 2)
 			# compute and draw the center (x, y)-coordinates of the
 			# ArUco marker
 			cX = int((topLeft[0] + bottomRight[0]) / 2.0)
 			cY = int((topLeft[1] + bottomRight[1]) / 2.0)
 			cv2.circle(frame, (cX, cY), 4, (0, 0, 255), -1)
 			# draw the ArUco marker ID on the frame
-			cv2.putText(frame, str(markerID),
+			fid_text = ""
+			if markerID == 0:
+				fid_text = "bottom_right"
+			elif markerID == 1:
+				fid_text = "top_left"
+			elif markerID == 2:
+				fid_text = "BLOCK"
+			elif markerID == 3:
+				fid_text = "bottom_left"
+			cv2.putText(frame, fid_text,
 				(topLeft[0], topLeft[1] - 15),
 				cv2.FONT_HERSHEY_SIMPLEX,
 				0.5, (0, 0, 0), 2)
